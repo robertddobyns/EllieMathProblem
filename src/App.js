5595bbd07c2d4e341c47f76a4d0553da66310e53
Robert Dobyns <robertddobyns@gmail.com>
2021-12-21 19:54:30 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import {Button, TextField} from '@mui/material';
+
 
 function App() {
+    const [field1, setField1] = useState('');
+    const [field2, setField2] = useState('');
+
+    const generateNumber = () => {
+        return Math.floor(Math.random() * 1000);
+    }
+
+    const generateProblems = () => {
+        setField1(generateNumber);
+        setField2(generateNumber);
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Button variant={'contained'} onClick={generateProblems}>Generate Problem</Button>
+
+        {field1 && <p>{field1}</p>}
+        {field2 && <p>{field2}</p>}
+        {field1 &&  <p>---------------</p>}
     </div>
   );
 }
